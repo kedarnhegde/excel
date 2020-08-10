a4a4a5d6e0b22ff3f2331b52a5bcb87b369ed935
kedarnhegde <kedarnhegde@gmail.com>
2020-08-10 17:58:44 +0530

    Initial Setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Main from './Components/prankPage';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render(){
+    return (
+      <div>
+        <BrowserRouter>
+          <Switch>
+            <Route path = '/' exact component={Main} />
+          </Switch>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Components/prankPage.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Prank extends Component {
+    render(){
+      return (
+        <div>
+          <h1>This is the Website</h1>
+        </div>
+      );
+    }
+  }
+  
+  export default Prank;
+  
